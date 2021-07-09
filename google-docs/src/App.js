86f5efcc17812d221368d05aa60cc1824fa26fd5
ScorchingShade <ankushors789@gmail.com>
2021-07-09 16:37:03 +0530

    added all features for first build

--- google-docs/src/App.js
+++ google-docs/src/App.js
@@ -1,15 +1,19 @@
-
-import './App.css';
-import TextEditor from './TextEditor.jsx';
-
-
-
+import "./App.css";
+import TextEditor from "./TextEditor.jsx";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {v4 as uuidV4} from "uuid"
 function App() {
   return (
-    <>
-  
-    <TextEditor/>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
